e5a8e06eb56994faf155f5d50e3e3714b97d7de6
Ananth Ganiga <ananth.ganiga@gmail.com>
2020-03-19 23:34:17 +0530

    Changed the batch size and improved some filtering

--- FindGoodStocksToBuy.py
+++ FindGoodStocksToBuy.py
@@ -4,26 +4,50 @@ import json
 
 #https://financialmodelingprep.com/developer/docs/#Stock-Price
 #https://financialmodelingprep.com/developer/docs/#Company-Quote
+#https://www.morningstar.com/stocks/screener-compare
+
 realtime_url = 'https://financialmodelingprep.com/api/v3/stock/real-time-price/'
 company_info_url = 'https://financialmodelingprep.com/api/v3/quote/'
 
+def giveCompanyList(companies, start, batchSize):
+    companyList = [ comp.strip() for comp in companies[start*batchSize:(start*batchSize)+batchSize]]
+    return ','.join(companyList)
+
+def extractTickerInfo():
+    pass
+##implement the extraction here  and return the  data Frame
+## Once all 51 batch done, then take the rejected tickers and call one by one
+
 with open('G:\WEBSCRAP\CompanyList.txt') as CompLists:
-    companies = CompLists.readlines()
+    companies = list(set(CompLists.readlines()))
 
 df = pd.DataFrame(columns = ['symbol', 'name', 'price', 'changesPercentage', 'change', 'dayLow', 'dayHigh', 'yearHigh', 'yearLow', 'marketCap', 'priceAvg50', 'priceAvg200', 'volume' , 'avgVolume', 'exhange', 'open', 'previousClose', 'eps', 'pe', 'earningsAnnouncement', 'sharesOutstanding', 'timestamp'])
+main_df = df.copy()
+exceptionTickers = list()
 
-for i in range(1,51):
-    for counter, company in enumerate(companies[:i*10]):
-        company = company.strip()
-        url =  company_info_url + company
-        price = requests.get(url)
-        priceinfo = price.text
-        # print("({}:{})".format(counter,company))
-        datadict = json.loads(priceinfo)
-        datadict = datadict[0]
-        df = df.append(datadict, ignore_index=True )
+for i in range(51):
+    companyList = giveCompanyList(companies, i ,10)
+    print("Extracting data for:", companyList )
+    url =  company_info_url + companyList
+    prices = requests.get(url)
+    prices = json.loads(prices.text)
 
-    df = df.drop(['earningsAnnouncement', 'timestamp'], axis=1)
-    print(df[['symbol', 'name', 'price', 'changesPercentage','dayHigh', 'yearHigh']])
-    df1 = df[ ( df['price'] <= df['yearHigh']*.6 ) and ( df['changesPercentage'] > -5.0 ) and ( df['price']< 50 ) ]
-    print(df1[['symbol', 'name', 'price', 'changesPercentage','dayHigh', 'yearHigh']])
+    for price in prices :
+        df = df.append(price, ignore_index=True )
+        df = df.drop(['earningsAnnouncement', 'timestamp'], axis=1)
+    try:
+        df = df[ ( df['price'] <= df['yearHigh']*.4 )]
+        df = df[ ( df['price']< 50 ) & ( (df['priceAvg50']/df['priceAvg200']).abs().round() == 1 ) ]
+        main_df = main_df.append(df)
+    except:  ##Improve the  error handling
+        exceptionTickers.append(companyList.split(','))
+        print("Exception while Filtering Shares:", companyList)
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+main_df = main_df.drop_duplicates() ##  Bad Code
+main_df = main_df.sort_values(['changesPercentage', 'marketCap'], ascending=[False, True])
+main_df['marketCap'] = (main_df['marketCap']/100000000).round().astype(str) + 'B'
+print(main_df[['symbol', 'name', 'price', 'changesPercentage','dayHigh', 'yearHigh', 'priceAvg50', 'priceAvg200', 'marketCap']])
